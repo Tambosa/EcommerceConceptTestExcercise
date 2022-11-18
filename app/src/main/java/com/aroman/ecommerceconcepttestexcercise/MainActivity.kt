ef6fcd23e49087cc002ab82f51b9905a14700273
Tambosa <tambosa@gmail.com>
2022-11-18 11:25:55 +0300

    setup for Koin (2)

--- app/src/main/java/com/aroman/ecommerceconcepttestexcercise/MainActivity.kt
+++ app/src/main/java/com/aroman/ecommerceconcepttestexcercise/MainActivity.kt
@@ -4,12 +4,8 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import com.aroman.data.repository.MockyRepositoryImpl
-import com.aroman.data.repository.retrofit.MockyApi
-import com.aroman.data.repository.retrofit.RetrofitClient
 import com.aroman.domain.repository.MockyRepository
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
@@ -22,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        lifecycleScope.launch{
+        lifecycleScope.launch {
             Log.d("@@@", "\ngetHomePage\n" + repo.getHomePage().toString())
             Log.d("@@@", "\ngetCartContents\n" + repo.getCartContents().toString())
             Log.d("@@@", "\ngetPhoneDetails\n" + repo.getPhoneDetails(1).toString())
